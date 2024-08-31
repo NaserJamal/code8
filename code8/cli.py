86335d61d47ae3896d4f0425e8158cfa34418846
NaserJamal <naser.dll@hotmail.com>
2024-08-31 04:42:33 +0300

    code8

--- code8/cli.py
+++ code8/cli.py
@@ -1,10 +1,10 @@
 import sys
 import argparse
-from .test_generator import generate_tests
-from .config_ui import run_config_ui
+from code8.test_generator import generate_tests
+from code8.config_ui import run_config_ui
 
 def main():
-    parser = argparse.ArgumentParser(description="Generate unit tests for Python files or configure settings.")
+    parser = argparse.ArgumentParser(description="Generate unit tests for Python files and more, or configure settings.")
     parser.add_argument('file', nargs='?', help="The Python file to generate tests for")
 
     args = parser.parse_args()
